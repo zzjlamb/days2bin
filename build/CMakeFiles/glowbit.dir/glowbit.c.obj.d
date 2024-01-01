cfb9ad533c8eadfeea480009f3094632f28c555d
zzjlamb <zzjlamb@me.com>
2024-01-01 07:59:00 +0000

    First attempt at scrolling text

--- build/CMakeFiles/glowbit.dir/glowbit.c.obj.d
+++ build/CMakeFiles/glowbit.dir/glowbit.c.obj.d
@@ -64,5 +64,5 @@ CMakeFiles/glowbit.dir/glowbit.c.obj: \
  /home/john/pico/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
  /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
  /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
- /home/john/mypicow/glowbit/ws2812.pio.h /usr/include/newlib/malloc.h \
- /usr/include/newlib/machine/malloc.h
+ /home/john/mypicow/glowbit/ws2812.pio.h \
+ /home/john/mypicow/glowbit/font.h

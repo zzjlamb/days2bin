423500a33a2418d74ccf92cd46f370e710e994a7
zzjlamb <zzjlamb@me.com>
2024-01-01 01:18:27 +0000

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   CMakeLists.txt 	new file:   glowbit.c 	new file:   pico_sdk_import.cmake 	new file:   ws2812.pio 	new file:   ws2812.pio.h

---
+++ build/CMakeFiles/glowbit.dir/glowbit.c.obj.d
@@ -0,0 +1,67 @@
+CMakeFiles/glowbit.dir/glowbit.c.obj: \
+ /home/john/mypicow/glowbit/glowbit.c /usr/include/newlib/stdio.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /usr/include/newlib/alloca.h \
+ /home/john/pico/pico-sdk/src/common/pico_stdlib/include/pico/stdlib.h \
+ /home/john/pico/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/john/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/john/pico/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
+ /home/john/mypicow/glowbit/build/generated/pico_base/pico/version.h \
+ /home/john/pico/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/john/mypicow/glowbit/build/generated/pico_base/pico/config_autogen.h \
+ /home/john/pico/pico-sdk/src/boards/include/boards/pico_w.h \
+ /home/john/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/john/pico/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /home/john/pico/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/john/pico/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/john/pico/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ /home/john/pico/pico-sdk/src/common/pico_time/include/pico/time.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_gpio/include/hardware/gpio.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/sio.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/interp.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/padsbank0.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pads_bank0.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/iobank0.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/io_bank0.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_irq/include/hardware/irq.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/m0plus.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_uart/include/hardware/uart.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/uart.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/uart.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/dreq.h \
+ /home/john/pico/pico-sdk/src/rp2_common/pico_stdio_uart/include/pico/stdio_uart.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_pio/include/hardware/pio.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/pio.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pio.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_pio/include/hardware/pio_instructions.h \
+ /home/john/pico/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /home/john/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
+ /home/john/mypicow/glowbit/ws2812.pio.h
